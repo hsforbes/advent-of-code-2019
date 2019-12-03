83e43406c9c7a459111a658786da0b96e0fddcfd
Heather Forbes <heather_forbes@homedepot.com>
2019-12-03 15:34:11 -0600

    Complete 02a.

--- 02a/02a.go
+++ 02a/02a.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Entry point
@@ -27,6 +29,41 @@ func main() {
 		}
 
 		fmt.Printf("%s \n", line)
+
+		inputStrings := strings.Split(line, ",")
+		inputInts := makeIntInput(inputStrings)
+
+		runIntcode(inputInts, 0)
+		// 234699 too low
+	}
+}
+
+func runIntcode(program []int, opcodePosition int) []int {
+	opcode := program[opcodePosition]
+
+	if opcode == 99 {
+		fmt.Println("Found opcode 99. Stopping. Final program state:")
+		fmt.Println(program)
+		return program
+	} else if opcode == 1 {
+		program[program[opcodePosition+3]] = program[program[opcodePosition+1]] + program[program[opcodePosition+2]]
+	} else if opcode == 2 {
+		program[program[opcodePosition+3]] = program[program[opcodePosition+1]] * program[program[opcodePosition+2]]
+	} else {
+		fmt.Println("It a sploded")
 	}
 
+	return runIntcode(program, opcodePosition+4)
+}
+
+func makeIntInput(inputStrings []string) []int {
+	inputInts := make([]int, len(inputStrings))
+	var err error
+	for i := 0; i < len(inputStrings); i++ {
+		inputInts[i], err = strconv.Atoi(inputStrings[i])
+	}
+	if err != nil {
+		panic(err)
+	}
+	return inputInts
 }

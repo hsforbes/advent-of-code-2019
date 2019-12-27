01fdc4925cdba3af4f42a35c0ca07971c60e8276
Heather Forbes <heather_forbes@homedepot.com>
2019-12-27 16:00:14 -0600

    Add readLine() to template.

--- templates/loop-thru-file.go
+++ templates/loop-thru-file.go
@@ -19,14 +19,21 @@ func main() {
 	reader := bufio.NewReader(file)
 
 	for {
-		lineBytes, _, err := reader.ReadLine()
-		line := string(lineBytes)
-
-		if err == io.EOF {
-			break
-		}
+		
+		line := readLine(reader)
 
 		fmt.Printf("%s \n", line)
 	}
 
+}
+
+func readLine(reader *bufio.Reader) string {
+
+	lineBytes, _, err := reader.ReadLine()
+	line := string(lineBytes)
+
+	if err == io.EOF {
+		panic(err)
+	}
+	return line
 }
